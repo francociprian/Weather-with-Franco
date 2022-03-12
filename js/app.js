1531ceee58dcf3f0d89fc5af44663b196fe81609
Franco Ciprian <franco_ciprian@hotmail.com>
2022-03-12 10:30:28 -0300

    APP CLIMA

--- js/app.js
+++ js/app.js
@@ -1,63 +1,44 @@
-$(window).scroll(function () {
-    if ($(document).scrollTop() > 950) {
-        $('.animation').addClass('animation--active');
-        $('.menu__btn').addClass('menu__btn-column')
-        console.log("OK");
+  // ba8865f37b122620faa7acdc436cbf69
+
+  let btn = document.querySelector("#btnObtenerClima");
+
+  btn.addEventListener("click", () => {
+    let city = document.querySelector("#cityname").value;
+
+    let key = "ba8865f37b122620faa7acdc436cbf69";
+    let url =
+      "http://api.openweathermap.org/data/2.5/weather?q=" +
+      city +
+      "&appid=" +
+      key;
+    //console.log(url);
+    if (city != "") {
+      fetch(url)
+        .then((data) => {
+          return data.json();
+        })
+        .then((clima) => {
+          console.log(clima);
+          let temperatura = clima.main.temp;
+          console.log(temperatura);
+          let tempC = temperatura - 273.15;
+          console.log(tempC);
+
+          let p = document.querySelector("#temperatura");
+          p.innerHTML = tempC.toFixed(0) + "C /";
+
+          if (tempC < 10) {
+            p.className = "cold";
+          } else {
+            p.className = "hot";
+          }
+        })
+        .catch((err) => console.log(err));
     } else {
-        $('.animation').removeClass('animation--active');
-        $('.menu__btn').removeClass('menu__btn-column')
+      Swal.fire({
+        icon: 'error',
+        title: 'Oops...',
+        text: 'Completar Ciudad'
+      })
     }
-});
-
-let modalFavoritos = document.getElementById('modalFavoritos');
-let btnFavoritos = document.getElementById('btnFavoritos');
-let btnClose = document.getElementsByClassName('close')[0];
-
-btnFavoritos.onclick = function() {
-    modalFavoritos.style.display = "block";
-}
-
-btnClose.onclick = function() {
-    modalFavoritos.style.display = "none";
-}
-
-window.onclick = function(event) {
-    if (event.target == modalFavoritos) {
-        modalFavoritos.style.display = "none";
-    }
-}
-
-/*
-
-let procesoScroll = document.documentElement.scrollTop;
-window.onscroll = function() {scrollFunction()};
-
-function scrollFunction() {
-  if (document.body.scrollTop > 100 || document.documentElement.scrollTop > 100) {
-    document.getElementById("scrollPag").style.color = "red";
-    console.log(procesoScroll);
-  } else {
-    document.getElementById("scrollPag").style.top = "blue";
-  }
-}
-
-*/
-
-/*
-//Como anidar el json al proyecto
-//--------------------------------------------------------
-document.addEventListener('DOMContentLoaded', () => {
-  fetchData();
-});
-
-const fetchData = async () => {
-  try {
-      const res = await fetch('api.json');
-      const data = await res.json();
-      console.log(data);
-  } catch (error) {
-    console.log(error);
-  }
-}
-//--------------------------------------------------------
-*/
+  });
